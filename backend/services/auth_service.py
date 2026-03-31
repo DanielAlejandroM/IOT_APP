9358d8d9aab6b0afc00b70d830b8b14987d8e5d9
Christian Miranda Z <cmiranda@dinersclub.com.ec>
2026-03-31 00:12:56 -0500

    CRMZ: Creaciòn de login con el token

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -2,6 +2,10 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from models.database import User
 
+from datetime import datetime, timedelta
+from jose import jwt
+from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
@@ -27,3 +31,30 @@ def create_user(db: Session, email: str, password: str):
     db.refresh(user)
 
     return user
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        return None
+
+    if not verify_password(password, user.password_hash):
+        return None
+
+    return user
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
