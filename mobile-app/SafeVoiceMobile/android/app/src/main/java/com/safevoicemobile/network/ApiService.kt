d5f68df372ee6c519463c93d6853c159c62723e7
Francisco <panchosye92@gmail.com>
2026-04-05 20:47:42 -0500

    EFJA:Conexion de modelo CNN con servicio nativo de kotlin

--- mobile-app/SafeVoiceMobile/android/app/src/main/java/com/safevoicemobile/network/ApiService.kt
+++ mobile-app/SafeVoiceMobile/android/app/src/main/java/com/safevoicemobile/network/ApiService.kt
@@ -1,16 +1,20 @@
 package com.safevoicemobile.network
 
-import com.safevoicemobile.network.dto.AlertRequest
+import com.safevoicemobile.network.dto.CommentRequest
+import com.safevoicemobile.network.dto.GeoRequest
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("alerts")
-    suspend fun createAlert(
-        @Header("Authorization") authorization: String?,
-        @Body request: AlertRequest
+    @POST("/comments")
+    suspend fun createComment(
+        @Body request: CommentRequest
+    ): Response<Unit>
+
+    @POST("/geo")
+    suspend fun createGeoEvent(
+        @Body request: GeoRequest
     ): Response<Unit>
 }
