7b6833d3a39bcda378dc50a46b2e0d6708321e1d
Christian Miranda Z <cmiranda@dinersclub.com.ec>
2026-04-08 01:35:12 -0500

    CRMZ:refactor formato de fechas y hora Ecuador

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,30 +1,28 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean
 from database.connection import Base
 from datetime import datetime
+from zoneinfo import ZoneInfo  # <-- clave
+
+ECUADOR_TZ = ZoneInfo("America/Guayaquil")
+
+
+def ecuador_now():
+    return datetime.now(ECUADOR_TZ)
 
 
 class User(Base):
     __tablename__ = "users"
 
-    # ==============================
-    # PRIMARY KEY
-    # ==============================
     id = Column(Integer, primary_key=True, index=True)
 
-    # ==============================
-    # USER DATA
-    # ==============================
     email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
     usuario = Column(String, nullable=False)
 
-    # ==============================
-    # (OPCIONAL MVP+)
-    # ==============================
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=ecuador_now)
     is_active = Column(Boolean, default=True)
     monitoring_active = Column(Boolean, default=False)
-    
+
 
 class Alert(Base):
     __tablename__ = "alerts"
@@ -36,6 +34,6 @@ class Alert(Base):
     lat = Column(Float, nullable=False)
     lng = Column(Float, nullable=False)
 
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    timestamp = Column(DateTime, default=ecuador_now)
 
     user_id = Column(Integer, ForeignKey("users.id"))
